13f14932a6f063ab87c4eeb317eea8e6347e5d3f
drpika1134 <drpika1134@gmail.com>
2019-09-02 19:31:47 -0600

    fixed string related errors

--- commands/target.js
+++ commands/target.js
@@ -88,7 +88,7 @@ module.exports.run = (bot, message, args) => {
     var dollar2 = dollar.substring(0, dollar.length - 1)
     sheets.spreadsheets.values.append({
       spreadsheetId: process.env.sheetID,
-      range: 'Targets!A2:N',
+      range: 'Targets!A2:N2',
       valueInputOption: 'USER_ENTERED',
       insertDataOption: 'INSERT_ROWS',
       resource: {
